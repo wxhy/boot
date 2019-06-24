db4e1fffe3c5169227bf4c36af548ce5a45823a1
qiuhongyun <qiuhongyun@jinxunhuan.com>
2019-06-24 09:42:55 +0800

    docker 批处理文件

--- bin/docker-build.bat
+++ bin/docker-build.bat
@@ -1,26 +1,26 @@
 rem /**********  docker pus **********/
 cd ..
 cd boot-auth
-mvn dockerfile:build -f pom.xml
+call mvn dockerfile:build -f pom.xml
 
 cd ..
 cd boot-config
-mvn dockerfile:build -f pom.xml
+call mvn dockerfile:build -f pom.xml
 
 
 cd ..
 cd boot-eureka
-mvn dockerfile:build -f pom.xml
+call mvn dockerfile:build -f pom.xml
 
 cd ..
 cd boot-gateway
-mvn dockerfile:build -f pom.xml
+call mvn dockerfile:build -f pom.xml
 
 cd ..
 cd boot-modules/boot-activiti
-mvn dockerfile:build -f pom.xml
+call mvn dockerfile:build -f pom.xml
 
 cd .. && cd ..
 cd boot-upms/boot-upms-server
-mvn dockerfile:build -f pom.xml
+call mvn dockerfile:build -f pom.xml
 
